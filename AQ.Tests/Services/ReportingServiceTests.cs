ed34ec14a19eb91a9bbb41a94d83c67b2541443d
yamesant <165981998+yamesant@users.noreply.github.com>
2025-02-03 22:26:12 +1000

    Parameterise the test

--- AQ.Tests/Services/ReportingServiceTests.cs
+++ AQ.Tests/Services/ReportingServiceTests.cs
@@ -21,25 +21,40 @@ public class ReportingServiceTests : DbTestsBase
         _dataContext.AchievementClasses.Add(_achievementClass);
         _dataContext.SaveChanges();
     }
+    
+    public static IEnumerable<object[]> GetQuantities()
+    {
+        yield return [
+            (int[][]) [[1], [2], [4], [8], [16], [32], [64]],
+            new SummaryStatisticsSnapshot(64, 32, 127),
+        ];
+        yield return [
+            (int[][]) [[10000], [1, 1], [], [], [], [2], [], [1, 3, 1]],
+            new SummaryStatisticsSnapshot(5, 0, 9),
+        ];
+    }
 
-    [Fact]
-    public async Task ShouldGetCorrectStatistics()
+    [Theory]
+    [MemberData(nameof(GetQuantities))]
+    public async Task ShouldGetCorrectStatistics(int[][] quantities, SummaryStatisticsSnapshot expected)
     {
         // Arrange
-        for (int i = 0; i < 7; i++)
+        for (int i = 0; i < quantities.Length; i++)
         {
             _timeProvider.Advance(TimeSpan.FromDays(1));
-            Achievement achievement = new()
+            DateOnly date = DateOnly.FromDateTime(_timeProvider.GetUtcNow().Date);
+            foreach (int quantity in quantities[i])
             {
-                CompletedDate = DateOnly.FromDateTime(_timeProvider.GetUtcNow().Date),
-                Quantity = 1 << i,
-                AchievementClass = _achievementClass,
-            };
-            _dataContext.Achievements.Add(achievement);
-            await _dataContext.SaveChangesAsync();
+                Achievement achievement = new()
+                {
+                    CompletedDate = date,
+                    Quantity = quantity,
+                    AchievementClass = _achievementClass,
+                };
+                _dataContext.Achievements.Add(achievement);
+                await _dataContext.SaveChangesAsync();
+            }
         }
-
-        SummaryStatisticsSnapshot expected = new(64, 32, 127);
         
         // Act
         SummaryStatisticsSnapshot result = await _reportingService.GetSummaryStatisticsSnapshot();
